038af68ec1688068e785703f989f0df961751d51
Brandon Gandy <tallain@gmail.com>
2025-06-07 12:03:54 -0700

    initial update

---
+++ main.py
@@ -0,0 +1,59 @@
+import tcod
+
+from engine import Engine
+from entity import Entity
+from input_handlers import EventHandler
+from procgen import generate_dungeon
+
+
+def main() -> None:
+    screen_width = 80
+    screen_height = 50
+
+    map_width = 80
+    map_height = 45
+
+    room_max_size = 10
+    room_min_size = 6
+    max_rooms = 30
+
+    tileset = tcod.tileset.load_tilesheet(
+        "dejavu10x10_gs_tc.png", 32, 8, tcod.tileset.CHARMAP_TCOD
+    )
+
+    event_handler = EventHandler()
+
+    player = Entity(int(screen_width / 2), int(screen_height / 2), "@", (255, 255, 255))
+    npc = Entity(int(screen_width / 2), int(screen_height / 2), "@", (255, 255, 0))
+    entities = {npc, player}
+
+    game_map = generate_dungeon(
+        max_rooms,
+        room_min_size,
+        room_max_size,
+        map_width,
+        map_height,
+        player
+    )
+
+    engine = Engine(entities=entities, event_handler=event_handler, game_map=game_map, player=player)
+
+    with tcod.context.new_terminal(
+        columns=screen_width,
+        rows=screen_height,
+        tileset=tileset,
+        title="Yet another Roguelike",
+        vsync=True
+    ) as context:
+        # order="F" changes 2D array access from [y,x] to [x,y]
+        root_console = tcod.console.Console(screen_width, screen_height, order="F")
+        while True:  # GAME LOOP
+            engine.render(console=root_console, context=context)
+
+            events = tcod.event.wait()
+
+            engine.handle_events(events)
+
+
+if __name__ == "__main__":
+    main()
---
+++ procgen.py
@@ -0,0 +1,113 @@
+from __future__ import annotations
+
+import random
+from typing import Iterator, List, Tuple, TYPE_CHECKING
+
+import tcod
+
+from game_map import GameMap
+import tile_types
+
+
+if TYPE_CHECKING:
+    from entity import Entity
+
+
+class RectangularRoom:
+    def __init__(self, x: int, y: int, width: int, height: int):
+        self.x1 = x
+        self.y1 = y
+        self.x2 = x + width
+        self.y2 = y + height
+
+    @property
+    def center(self) -> Tuple[int, int]:
+        center_x = int((self.x1 + self.x2) / 2)
+        center_y = int((self.y1 + self.y2) / 2)
+
+        return center_x, center_y
+
+    @property
+    def inner(self) -> Tuple[slice, slice]:
+        """
+        :return: The inner area of this room as a 2D array index.
+        """
+        return slice(self.x1 + 1, self.x2), slice(self.y1 + 1, self.y2)
+
+    def intersects(self, other: RectangularRoom) -> bool:
+        """
+        :param other: The room to check for overlap.
+        :return: True if this room overlaps with the other room.
+        """
+        return (
+            self.x1 <= other.x2
+            and self.x2 >= other.x1
+            and self.y1 <= other.y2
+            and self.y2 >= other.y1
+        )
+
+
+def tunnel_between(
+        start: Tuple[int, int],
+        end: Tuple[int, int]
+) -> Iterator[Tuple[int, int]]:
+    """
+    Return an L-shaped tunnel between the points provided.
+    :param start:
+    :param end:
+    :return:
+    """
+    x1, y1 = start
+    x2, y2 = end
+    if random.random() < 0.5:
+        # Move horizontally, then vertically
+        corner_x, corner_y = x2, y1
+    else:
+        # Move vertically, then horizontally
+        corner_x, corner_y = x1, y2
+
+    # Generate tunnel coordinates.
+    for x, y in tcod.los.bresenham((x1, y1), (corner_x, corner_y)).tolist():
+        yield x, y
+    for x, y in tcod.los.bresenham((corner_x, corner_y), (x2, y2)):
+        yield x, y
+
+
+def generate_dungeon(
+        max_room_attempts: int,
+        room_min_size: int,
+        room_max_size: int,
+        map_width: int,
+        map_height: int,
+        player: Entity
+) -> GameMap:
+    dungeon = GameMap(map_width, map_height)
+
+    rooms: List[RectangularRoom] = []
+
+    for r in range(max_room_attempts):
+        room_width = random.randint(room_min_size, room_max_size)
+        room_height = random.randint(room_min_size, room_max_size)
+
+        x = random.randint(0, dungeon.width - room_width - 1)
+        y = random.randint(0, dungeon.height - room_height - 1)
+
+        # RectangularRoom class makes rects easier to work with
+        new_room = RectangularRoom(x, y, room_width, room_height)
+
+        # Run through other rooms to see if they intersect with this one.
+        if any(new_room.intersects(other_room) for other_room in rooms):
+            continue  # Try again
+
+        dungeon.tiles[new_room.inner] = tile_types.floor
+
+        # Player starts in first room
+        if len(rooms) == 0:
+            player.x, player.y = new_room.center
+        else:
+            for x, y in tunnel_between(rooms[-1].center, new_room.center):
+                dungeon.tiles[x, y] = tile_types.floor
+
+        rooms.append(new_room)
+
+    return dungeon
