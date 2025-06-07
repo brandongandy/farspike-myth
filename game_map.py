c0ecfea877bdd02035088ada22e9b9e3a73f28eb
Brandon Gandy <tallain@gmail.com>
2025-06-07 12:26:30 -0700

    add FOV

--- game_map.py
+++ game_map.py
@@ -9,6 +9,12 @@ class GameMap:
         self.width, self.height = width, height
         self.tiles = np.full((width, height), fill_value=tile_types.wall, order="F")
 
+        # Tiles the player can currently see
+        self.visible = np.full((width, height), fill_value=False, order="F")
+
+        # Tiles the player has seen before, but are not currently visible
+        self.explored = np.full((width, height), fill_value=False, order="F")
+
         self.tiles[30:33, 22] = tile_types.wall
 
     def in_bounds(self, x: int, y: int) -> bool:
@@ -21,4 +27,14 @@ class GameMap:
         return 0 <= x < self.width and 0 <= y < self.height
 
     def render(self, console: Console) -> None:
-        console.tiles_rgb[0:self.width, 0:self.height] = self.tiles["dark"]
+        """
+        Renders the map. If a tile is visible, then it's drawn with the "light" color.
+        Otherwise, if it's explored, it's drawn "dark." All others are in the SHROUD.
+        :param console:
+        :return:
+        """
+        console.tiles_rgb[0:self.width, 0:self.height] = np.select(
+            condlist=[self.visible, self.explored],
+            choicelist=[self.tiles["light"], self.tiles["dark"]],
+            default=tile_types.SHROUD
+        )
