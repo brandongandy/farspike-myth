646fe0f798f1ed3c7d61b01a495763b7ab0727ea
Brandon Gandy <tallain@gmail.com>
2025-06-08 08:45:23 -0700

    set the world up for smacking monsters

--- actions.py
+++ actions.py
@@ -23,13 +23,29 @@ class EscapeAction(Action):
         raise SystemExit()
 
 
-class MovementAction(Action):
+class ActionWithDirection(Action):
     def __init__(self, dx: int, dy: int):
         super().__init__()
 
         self.dx = dx
         self.dy = dy
 
+    def perform(self, engine: Engine, entity: Entity) -> None:
+        raise NotImplementedError()
+
+
+class MeleeAction(ActionWithDirection):
+    def perform(self, engine: Engine, entity: Entity) -> None:
+        dest_x = entity.x + self.dx
+        dest_y = entity.y + self.dy
+        target = engine.game_map.get_blocking_entity_at_location(dest_x, dest_y)
+        if not target:
+            return
+
+        print(f"you kick the {target.name}")
+
+
+class MovementAction(ActionWithDirection):
     def perform(self, engine: Engine, entity: Entity) -> None:
         dest_x = entity.x + self.dx
         dest_y = entity.y + self.dy
@@ -38,5 +54,18 @@ class MovementAction(Action):
             return  # Destination out of bounds!
         if not engine.game_map.tiles["walkable"][dest_x, dest_y]:
             return  # We're blocked!
+        if engine.game_map.get_blocking_entity_at_location(dest_x, dest_y):
+            return  # Block by an entity.
 
         entity.move(self.dx, self.dy)
+
+
+class BumpAction(ActionWithDirection):
+    def perform(self, engine: Engine, entity: Entity) -> None:
+        dest_x = entity.x + self.dx
+        dest_y = entity.y + self.dy
+
+        if engine.game_map.get_blocking_entity_at_location(dest_x, dest_y):
+            return MeleeAction(self.dx, self.dy).perform(engine, entity)
+        else:
+            return MovementAction(self.dx, self.dy).perform(engine, entity)
